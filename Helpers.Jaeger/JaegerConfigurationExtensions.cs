d83ab4d0bfe122ae77d1f5e99dc4e604b2f73ec9
Bob Hall <myk_quayce@hotmail.com>
2019-05-05 20:20:50 +0100

    adding jaeger and tracing projects

---
+++ Helpers.Jaeger/JaegerConfigurationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Jaeger;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders;
+using OpenTracing;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class JaegerConfigurationExtensions
+	{
+		public static IServiceCollection AddJaegerTracing(this IServiceCollection services, string serviceName, string host = "localhost", int port = 6831)
+		{
+			if (string.IsNullOrWhiteSpace(serviceName)) throw new ArgumentNullException(nameof(serviceName));
+
+			var sender = new UdpSender(host, port, maxPacketSize: 0);
+
+			var reporter = new RemoteReporter.Builder()
+				.WithSender(sender)
+				.Build();
+
+			var sampler = new ConstSampler(sample: true);
+
+			var tracer = new Tracer.Builder(serviceName)
+				.WithReporter(reporter)
+				.WithSampler(sampler)
+				.Build();
+
+			services
+				.AddOpenTracing()
+				.AddSingleton<ITracer>(tracer);
+
+			return services;
+		}
+	}
+}
---
+++ Helpers.Tracing/ExtensionMethods.cs
@@ -0,0 +1,51 @@
+﻿using OpenTracing;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Helpers.Tracing
+{
+	public static class ExtensionMethods
+	{
+		public static string ReducePath(this string path)
+		{
+			if (string.IsNullOrWhiteSpace(path))
+			{
+				return string.Empty;
+			}
+
+			var segments = path.Split(Path.DirectorySeparatorChar);
+
+			return segments[segments.Length - 1];
+		}
+
+		public static ISpanBuilder BuildDefaultSpan(
+			this ITracer tracer,
+			[CallerFilePath] string filePath = default,
+			[CallerMemberName] string methodName = default)
+		{
+			var path = filePath.ReducePath();
+
+			var operationName = string.Concat(path, "=>", methodName);
+
+			return tracer.BuildSpan(operationName);
+		}
+
+		public static ISpan Log(this ISpan span, params object[] values)
+		{
+			var dictionary = new Dictionary<string, object>(values.Length / 2);
+
+			for (var a = 0; a < values.Length; a += 2)
+			{
+				var key = values[a].ToString();
+				var value = values[a + 1];
+
+				dictionary[key] = value;
+			}
+
+			span.Log(dictionary);
+
+			return span;
+		}
+	}
+}
