df3a721b2dccd161d2fac7799c518aeacdadfda6
Bob Hall <myk_quayce@hotmail.com>
2019-07-12 23:10:57 +0100

    removing newtonsoft.json

--- Helpers.Telegram.Models/Generated/From.cs
+++ Helpers.Telegram.Models/Generated/From.cs
@@ -1,13 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
 
 namespace Helpers.Telegram.Models.Generated
 {
 	public class From
 	{
 		public int Id { get; set; }
-		[JsonProperty("is_bot")]
+		[JsonPropertyName("is_bot")]
 		public bool IsBot { get; set; }
-		[JsonProperty("first_name")]
+		[JsonPropertyName("first_name")]
 		public string? FirstName { get; set; }
 		public string? Username { get; set; }
 	}
