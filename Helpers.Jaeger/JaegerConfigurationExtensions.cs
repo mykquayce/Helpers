28891ad73d9ede42e966aa703d32e159952a2cb1
Bob Hall <myk_quayce@hotmail.com>
2019-08-26 22:59:57 +0100

    adding jaeger overloads

--- Helpers.Jaeger/JaegerConfigurationExtensions.cs
+++ Helpers.Jaeger/JaegerConfigurationExtensions.cs
@@ -1,7 +1,10 @@
-﻿using Jaeger;
+﻿using Dawn;
+using Helpers.Jaeger.Models;
+using Jaeger;
 using Jaeger.Reporters;
 using Jaeger.Samplers;
 using Jaeger.Senders;
+using Microsoft.Extensions.Configuration;
 using OpenTracing;
 using System;
 
@@ -9,13 +12,41 @@ namespace Microsoft.Extensions.DependencyInjection
 {
 	public static class JaegerConfigurationExtensions
 	{
-		public static IServiceCollection AddJaegerTracing(this IServiceCollection services, string serviceName, string host = "localhost", int port = 6831)
+		public static IServiceCollection AddJaegerTracing(this IServiceCollection services, IConfiguration configuration)
 		{
+			Guard.Argument(() => configuration).NotNull();
+
+			var serviceName = Guard.Argument(() => configuration["serviceName"]).NotNull().NotEmpty().NotWhiteSpace().Value;
+			var host = Guard.Argument(() => configuration["host"]).NotNull().NotEmpty().NotWhiteSpace().Value;
+			var port = Guard.Argument(() => configuration["port"]).NotNull().NotEmpty().NotWhiteSpace().Matches(@"^\d{1,5}$").Cast<int>().Value;
+
+			return AddJaegerTracing(services, serviceName, host, port);
+		}
+
+		public static IServiceCollection AddJaegerTracing(this IServiceCollection services, Settings settings)
+		{
+			Guard.Argument(() => settings).NotNull();
+			Guard.Argument(() => settings.ServiceName).NotNull();
+
+			return AddJaegerTracing(services, settings.ServiceName!, settings.Host, settings.Port);
+		}
+
+		public static IServiceCollection AddJaegerTracing(
+			this IServiceCollection services,
+			string serviceName,
+			string? host = "localhost",
+			int? port = 6_831)
+		{
+			Guard.Argument(() => services).NotNull();
+			Guard.Argument(() => serviceName).NotNull().NotEmpty().NotWhiteSpace();
+			Guard.Argument(() => host).NotNull().NotEmpty().NotWhiteSpace();
+			Guard.Argument(() => port).NotNull().InRange(1, 65_535);
+
 			try
 			{
 				if (string.IsNullOrWhiteSpace(serviceName)) throw new ArgumentNullException(nameof(serviceName));
 
-				var sender = new UdpSender(host, port, maxPacketSize: 0);
+				var sender = new UdpSender(host, port!.Value, maxPacketSize: 0);
 
 				var reporter = new RemoteReporter.Builder()
 					.WithSender(sender)
---
+++ Helpers.Jaeger/Models/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace Helpers.Jaeger.Models
+{
+	public class Settings
+	{
+		public string? ServiceName { get; set; }
+		public string? Host { get; set; } = "localhost";
+		public int? Port { get; set; } = 6_831;
+	}
+}
