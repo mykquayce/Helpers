29f8e9488ddf2ad08272822d7dca5881b9b92883
Bob Hall <myk_quayce@hotmail.com>
2019-10-03 20:41:14 +0100

    fixing bash variable name

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ dotnet build
 
 ls -1 */*.csproj | awk '{system("dotnet pack " $1 " --output ./nupkg")}'
 
-ls -1 ./nupkg/*.nupkg | awk -v ApiKey="$NuGetServerApiKey" '{system("nuget push " $1 " " ApiKey " -Source http://nuget/nuget")}'
+ls -1 ./nupkg/*.nupkg | awk -v ApiKey="$NUGET_SERVER_API_KEY" '{system("nuget push " $1 " " ApiKey " -Source http://nuget/nuget")}'
