f2e8fa62b5271e981319dba157c7930f889ffe56
Bob Hall <myk_quayce@hotmail.com>
2019-11-12 02:20:20 +0000

    making a central CinemaMovieShow POCO

---
+++ Helpers.Cineworld.Models/CinemaMovieShow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helpers.Cineworld.Models
+{
+	public class CinemaMovieShow
+	{
+		public string? Cinema { get; set; }
+		public DateTime DateTime { get; set; }
+		public TimeSpan End { get; set; }
+		public string? Movie { get; set; }
+	}
+}
