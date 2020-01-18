a63736d5ceb163e8889a77b838a1fd96dbf00806
bob hall <myk_quayce@hotmail.com>
2020-01-18 17:59:05 +0000

    adding Formats property to FilmType

---
+++ Helpers.Cineworld.Models.Tests/FilmTypeTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Helpers.Cineworld.Models.Tests
+{
+	public class FilmTypeTests
+	{
+		[Theory]
+		[InlineData("1917", "1917", Enums.Formats._2d)]
+		[InlineData("M4J A Shaun The Sheep Movie: Farmageddon", "Shaun The Sheep Movie: Farmageddon, A", Enums.Formats._2d | Enums.Formats.MoviesForJuniors)]
+		[InlineData("Bombshell : Unlimited Screening", "Bombshell", Enums.Formats._2d | Enums.Formats.SecretUnlimitedScreening)]
+		public void FilmTypeTests_Formats(string title, string expectedTitle, Enums.Formats expectedFormats)
+		{
+			// Arrange, Act
+			var film = new Generated.FilmType { Title = title, };
+
+			// Assert
+			Assert.Equal(expectedTitle, film.Title);
+			Assert.Equal(expectedFormats, film.Formats);
+		}
+	}
+}
