27c2586c3d779b84593de5cc638def9259e5cbf1
bob hall <myk_quayce@hotmail.com>
2020-02-15 16:21:08 +0000

    tweaking Query model to use IReadOnlyList

--- Helpers.Cineworld.Models/Query.cs
+++ Helpers.Cineworld.Models/Query.cs
@@ -5,12 +5,12 @@ namespace Helpers.Cineworld.Models
 {
 	public class Query
 	{
-		public IList<short> CinemaIds { get; set; } = new List<short>();
+		public IReadOnlyList<short> CinemaIds { get; set; } = new List<short>();
 		[JsonConverter(typeof(JsonStringFlagsEnumConverter<Enums.TimesOfDay>))]
 		public Enums.TimesOfDay TimesOfDay { get; set; } = Enums.TimesOfDay.AllDay;
 		[JsonConverter(typeof(JsonStringFlagsEnumConverter<Enums.DaysOfWeek>))]
 		public Enums.DaysOfWeek DaysOfWeek { get; set; } = Enums.DaysOfWeek.AllWeek;
 		public byte WeekCount { get; set; } = byte.MaxValue;
-		public IList<string> Titles { get; set; } = new List<string>();
+		public IReadOnlyList<string> Titles { get; set; } = new List<string>();
 	}
 }
