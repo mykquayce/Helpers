c910ad6fa4c6ad16ea5c3d51c7e4e6b2df1ae277
bob hall <myk_quayce@hotmail.com>
2020-02-18 19:18:04 +0000

    tests

---
+++ Helpers.Tracing.Tests/LogTests.cs
@@ -0,0 +1,113 @@
+﻿using Moq;
+using OpenTracing;
+using OpenTracing.Tag;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Helpers.Tracing.Tests
+{
+	public class LogTests
+	{
+		private readonly ISpan _span;
+		private readonly IDictionary<string, object?> _logsDictionary = new Dictionary<string, object?>(StringComparer.InvariantCultureIgnoreCase);
+
+		public LogTests()
+		{
+			var spanMock = new Mock<ISpan>(MockBehavior.Strict);
+
+			spanMock
+				.Setup(s => s.SetTag(It.IsAny<BooleanTag>(), It.IsAny<bool>()))
+				.Returns(spanMock.Object);
+
+			spanMock
+				.Setup(s => s.Log(It.IsAny<IEnumerable<KeyValuePair<string, object?>>>()))
+				.Callback<IEnumerable<KeyValuePair<string, object?>>>(kvps =>
+				{
+					foreach (var (key, value) in kvps)
+					{
+						_logsDictionary.Add(key, value);
+					}
+				})
+				.Returns(spanMock.Object);
+
+			_span = spanMock.Object;
+		}
+
+		[Theory]
+		[InlineData("key", "value")]
+		[InlineData("value", 0)]
+		public void Params(string key, object value)
+		{
+			_logsDictionary.Clear();
+
+			Assert.Empty(_logsDictionary);
+
+			_span.Log(key, value);
+
+			Assert.Single(_logsDictionary);
+			Assert.True(_logsDictionary.ContainsKey(key));
+			Assert.Equal(value, _logsDictionary[key]);
+		}
+
+		[Theory]
+		[InlineData("key", "value")]
+		[InlineData("value", 0)]
+		public void TupleParams(string key, object value)
+		{
+			_logsDictionary.Clear();
+
+			Assert.Empty(_logsDictionary);
+
+			_span.Log((key, value));
+
+			Assert.Single(_logsDictionary);
+			Assert.True(_logsDictionary.ContainsKey(key));
+			Assert.Equal(value, _logsDictionary[key]);
+		}
+
+		[Fact]
+		public void Exception()
+		{
+			// Arrange
+			_logsDictionary.Clear();
+
+			var innerException = new InvalidTimeZoneException("inner")
+			{
+				Data =
+				{
+					["three"] = 3,
+					["four"] = 4,
+				},
+			};
+
+			var outerException = new InvalidOperationException("outer", innerException)
+			{
+				Data =
+				{
+					["one"] = 1,
+					["two"] = 2,
+				},
+			};
+
+			Assert.Empty(_logsDictionary);
+
+			// Act
+			_span.Log(outerException);
+
+			// Assert
+			Assert.NotEmpty(_logsDictionary);
+			Assert.Contains(LogFields.ErrorKind, _logsDictionary.Keys);
+			Assert.Contains(LogFields.ErrorObject, _logsDictionary.Keys);
+			Assert.Contains(LogFields.Event, _logsDictionary.Keys);
+			Assert.Contains(LogFields.Message, _logsDictionary.Keys);
+			Assert.Contains(LogFields.Stack, _logsDictionary.Keys);
+			Assert.Contains(nameof(System.Exception.Data), _logsDictionary.Keys);
+			Assert.Equal("System.InvalidTimeZoneException", _logsDictionary[LogFields.ErrorKind]);
+			Assert.IsType<InvalidTimeZoneException>(_logsDictionary[LogFields.ErrorObject]);
+			Assert.Equal("error", _logsDictionary[LogFields.Event]);
+			Assert.Equal("inner", _logsDictionary[LogFields.Message]);
+			Assert.Equal("one=1;two=2;three=3;four=4;", _logsDictionary[nameof(System.Exception.Data)]);
+		}
+	}
+}
---
+++ Helpers.Tracing.Tests/MockSpanBuilder.cs
@@ -0,0 +1,33 @@
+﻿using OpenTracing;
+using OpenTracing.Tag;
+using System;
+
+namespace Helpers.Tracing.Tests
+{
+	public class MockSpanBuilder : ISpanBuilder
+	{
+		public MockSpanBuilder(string operationName)
+		{
+			OperationName = operationName;
+		}
+
+		public string OperationName { get; }
+
+		public ISpanBuilder AddReference(string referenceType, ISpanContext referencedContext) => throw new NotImplementedException();
+		public ISpanBuilder AsChildOf(ISpanContext parent) => throw new NotImplementedException();
+		public ISpanBuilder AsChildOf(ISpan parent) => throw new NotImplementedException();
+		public ISpanBuilder IgnoreActiveSpan() => throw new NotImplementedException();
+		public ISpan Start() => throw new NotImplementedException();
+		public IScope StartActive() => throw new NotImplementedException();
+		public IScope StartActive(bool finishSpanOnDispose) => throw new NotImplementedException();
+		public ISpanBuilder WithStartTimestamp(DateTimeOffset timestamp) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(string key, string value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(string key, bool value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(string key, int value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(string key, double value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(BooleanTag tag, bool value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(IntOrStringTag tag, string value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(IntTag tag, int value) => throw new NotImplementedException();
+		public ISpanBuilder WithTag(StringTag tag, string value) => throw new NotImplementedException();
+	}
+}
