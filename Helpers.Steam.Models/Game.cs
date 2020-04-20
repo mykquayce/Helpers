a9b2b3ae6734ff1f86304471997cd47e1b32d0fc
bob hall <myk_quayce@hotmail.com>
2020-04-20 03:08:18 +0100

    adding Minutes property to the Game POCO

--- Helpers.Steam.Models/Game.cs
+++ Helpers.Steam.Models/Game.cs
@@ -6,15 +6,9 @@ namespace Helpers.Steam.Models
 	{
 		[JsonPropertyName("appid")]
 		public int? AppId { get; set; }
-		[JsonPropertyName("playtime_2weeks")]
-		public int? Playtime2Weeks { get; set; }
 		[JsonPropertyName("playtime_forever")]
-		public int? PlaytimeForever { get; set; }
-		[JsonPropertyName("playtime_windows_forever")]
-		public int? PlaytimeWindowsForever { get; set; }
-		[JsonPropertyName("playtime_mac_forever")]
-		public int? FlaytimeMacForever { get; set; }
-		[JsonPropertyName("playtime_linux_forever")]
-		public int? PlaytimeLinuxForever { get; set; }
+		public int? Minutes { get; set; }
+		[JsonPropertyName("name")]
+		public string? Name { get; set; }
 	}
 }
