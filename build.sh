33e42db7ed197f0f6537c3d6a5f2e6bb7e235f97
bob hall <myk_quayce@hotmail.com>
2020-05-15 20:47:51 +0100

    ptdrnkrnpiftdkrpftindue

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ dotnet build
 
 dotnet pack --output ./nupkg
 
-ls -1 ./nupkg/*.nupkg | awk -v ApiKey="$NUGET_SERVER_API_KEY" '{system("dotnet nuget push " $1 " --api-key " ApiKey " --source http://nuget/nuget")}'
+ls -1 ./nupkg/*.nupkg \
+	| awk -v ApiKey="$NUGET_SERVER_API_KEY" '{system("dotnet nuget push " $1 " --api-key " ApiKey " --source http://nuget/nuget \
+	| head --lines=3")}'
