a7c0b10629233f8efc374937ffb2b242246c181c
bob hall <myk_quayce@hotmail.com>
2020-06-07 14:28:13 +0100

    removing old teamcity stuff; and tidying build script

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-dotnet restore -s https://api.nuget.org/v3/index.json -s http://nuget/nuget
+dotnet restore --source https://api.nuget.org/v3/index.json --source http://nuget/nuget
 
 dotnet build
 
 dotnet pack --output ./nupkg
 
-ls -1 ./nupkg/*.nupkg \
-	| awk -v ApiKey="$NUGET_SERVER_API_KEY" '{system("dotnet nuget push " $1 " --api-key " ApiKey " --source http://nuget/nuget \
-	| head --lines=3")}'
+if [ -n "$NuGetServerApiKey" ]
+then
+	for f in ./nupkg/*.nupkg
+	do
+		dotnet nuget push $f --api-key $NuGetServerApiKey --source http://nuget | head --lines=3
+	done
+end
