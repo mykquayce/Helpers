a5465ecab4d474724ae67b5a5ae12aa577462843
bob hall <myk_quayce@hotmail.com>
2020-07-26 13:42:06 +0100

    jwt helpers

---
+++ Helpers.Jwt.Tests/TokenFactoryTests.cs
@@ -0,0 +1,187 @@
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.Json;
+using Xunit;
+
+namespace Helpers.Jwt.Tests
+{
+	public class TokenFactoryTests
+	{
+		private const string _key = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEu";
+		private const string _issuer = "http://localhost";
+		private const string _audience = "audience";
+
+		private readonly TokenFactory _tokenFactory;
+
+		public TokenFactoryTests()
+		{
+			_tokenFactory = new TokenFactory(_key, _issuer, _audience);
+		}
+
+		[Theory]
+		[InlineData(Roles.Admin, default, 1_000)]
+		[InlineData(Roles.User, "test", 1_000)]
+		[InlineData(Roles.Admin | Roles.User, "test", 1_000)]
+		public void GenerateTest(Roles roles, string? name, int iterations = 100)
+		{
+			while (--iterations >= 0)
+			{
+				var token = _tokenFactory.Generate(roles, name);
+
+				TokenTests(token);
+			}
+		}
+
+		[Theory]
+		[InlineData(1)]
+		[InlineData(10)]
+		[InlineData(100)]
+		[InlineData(1_000)]
+		[InlineData(10_000)]
+		[InlineData(100_000)]
+		public void GenerateWithExpiryTest(int expirySeconds)
+		{
+			var now = DateTime.UtcNow;
+			var expiry = TimeSpan.FromSeconds(expirySeconds);
+
+			var token = _tokenFactory.Generate(Roles.Admin, default, expiry);
+
+			var parts = token.Split('.')
+				.Select(WebEncoders.Base64UrlDecode)
+				.Select(System.Text.Encoding.UTF8.GetString)
+				.ToList();
+
+			var payloadJson = parts[1];
+
+			var payload = JsonSerializer.Deserialize<PayloadObject>(payloadJson);
+
+			var actual = (payload!.ExpiryDateTime - now).TotalSeconds;
+
+			Assert.InRange(actual, expirySeconds - 1, expirySeconds + 1);
+		}
+
+		private static void TokenTests(string token)
+		{
+			var now = DateTime.UtcNow;
+
+			Assert.NotNull(token);
+			Assert.NotEmpty(token);
+			Assert.Matches(@"^[-0-9A-Z_a-z]+\.[-0-9A-Z_a-z]+\.[-0-9A-Z_a-z]+$", token);
+
+			var parts = token.Split('.')
+				.Select(WebEncoders.Base64UrlDecode)
+				.Select(System.Text.Encoding.UTF8.GetString)
+				.ToList();
+
+			var headerJson = parts[0];
+			var payloadJson = parts[1];
+			var signature = parts[2];
+
+			Assert.NotNull(headerJson);
+			Assert.NotEmpty(headerJson);
+			Assert.StartsWith("{", headerJson);
+			Assert.NotEqual("{}", headerJson);
+
+			var header = JsonSerializer.Deserialize<Dictionary<string, string>>(headerJson);
+
+			Assert.NotNull(header);
+			Assert.NotEmpty(header);
+			Assert.Contains("alg", header!.Keys);
+			Assert.Equal("http://www.w3.org/2001/04/xmldsig-more#hmac-sha256", header["alg"]);
+			Assert.Contains("typ", header.Keys);
+			Assert.Equal("JWT", header["typ"]);
+
+			Assert.NotNull(payloadJson);
+			Assert.NotEmpty(payloadJson);
+			Assert.StartsWith("{", payloadJson);
+			Assert.NotEqual("{}", payloadJson);
+
+			var payload = JsonSerializer.Deserialize<PayloadObject>(payloadJson);
+
+			Assert.NotNull(payload);
+			Assert.NotEmpty(payload);
+			Assert.InRange(payload!.ExpiryDateTime, now, now.AddDays(1));
+			Assert.Equal(_issuer, payload.Issuer);
+			Assert.Equal(_audience, payload.Audience);
+			Assert.NotEqual(default, payload.Roles);
+
+			Assert.NotNull(signature);
+			Assert.NotEmpty(signature);
+		}
+
+		public class PayloadObject : Dictionary<string, JsonElement>
+		{
+			private const string _expiryKey = "exp",
+				_audienceKey = "aud",
+				_issuerKey = "iss",
+				_rolesKey = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+				_nameKey = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+
+			private string GetString(string key) => this.TryGetValue(key, out var e)
+				? e.GetString() ?? throw new ArgumentNullException(key)
+				: throw new KeyNotFoundException($"{key} {nameof(key)} not found in " + string.Join(',', this.Keys));
+
+			public double Expiry => this.TryGetValue(_expiryKey, out var e) && e.ValueKind == JsonValueKind.Number
+				? e.TryGetDouble(out var d)
+					? d
+					: throw new ArgumentOutOfRangeException(_expiryKey, e.GetString(), $"Unparsable {_expiryKey}: {e} ")
+				: throw new KeyNotFoundException(_expiryKey);
+
+			public DateTime ExpiryDateTime => DateTime.UnixEpoch.AddSeconds(this["exp"].GetDouble());
+
+			public string Audience => GetString(_audienceKey);
+			public string Issuer => GetString(_issuerKey);
+
+			public Roles Roles => this.TryGetValue(_rolesKey, out var element)
+				? element.GetEnum<Roles>()
+				: throw new KeyNotFoundException(_rolesKey);
+
+			public string? Name => this.TryGetValue(_nameKey, out var element)
+				? element.GetString()
+				: default;
+		}
+	}
+
+	public static class JsonElementExtensions
+	{
+		public static T GetEnum<T>(this JsonElement element)
+			where T : struct, Enum, IConvertible
+		{
+			var provider = System.Globalization.CultureInfo.InvariantCulture;
+
+			switch (element.ValueKind)
+			{
+				case JsonValueKind.String:
+					{
+						var s = element.GetString() ?? throw new ArgumentNullException();
+
+						if (Enum.TryParse<T>(s, out var roles))
+						{
+							return roles;
+						}
+
+						throw new ArgumentOutOfRangeException(nameof(s), s, $"Unexpected role: {s}");
+					}
+				case JsonValueKind.Array:
+					{
+						var roles = 0L;
+
+						foreach (var role in from e in element.EnumerateArray()
+											 let t = e.GetEnum<T>()
+											 let i = t.ToInt64(provider)
+											 select i)
+						{
+							roles |= role;
+						}
+
+						return Enum.Parse<T>(roles.ToString("D"));
+					}
+				default:
+					throw new ArgumentOutOfRangeException(nameof(element.ValueKind), element.ValueKind, $"Unexpected {nameof(element.ValueKind)}: {element.ValueKind}.");
+			}
+		}
+	}
+}
---
+++ Helpers.Jwt/TokenFactory.cs
@@ -0,0 +1,69 @@
+﻿using Helpers.Common;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Helpers.Jwt
+{
+	public class TokenFactory
+	{
+		private static readonly TimeSpan _defaultExpiry = TimeSpan.FromMinutes(5);
+
+		private readonly JwtHeader _header;
+		private readonly string _issuer;
+		private readonly string _audience;
+
+		public TokenFactory(string key, string issuer, string audience)
+		{
+			var encodedKey = WebEncoders.Base64UrlDecode(key);
+			var securityKey = new SymmetricSecurityKey(encodedKey);
+			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+			_audience = audience;
+			_header = new JwtHeader(credentials);
+			_issuer = issuer;
+		}
+
+		public string Generate(Roles roles = Roles.Admin, string? name = default, TimeSpan? expiry = default)
+		{
+			var expirySeconds = (int)DateTime.UtcNow
+				.Add(expiry ?? _defaultExpiry)
+				.Subtract(DateTime.UnixEpoch)
+				.TotalSeconds;
+
+			var payload = new JwtPayload
+			{
+				["exp"] = expirySeconds,
+				["iss"] = _issuer,
+				["aud"] = _audience,
+			};
+
+			payload.AddClaims(from r in roles.Expand()
+							  let s = r.ToString("G")
+							  let c = new Claim(ClaimTypes.Role, s)
+							  select c);
+
+			if (!string.IsNullOrWhiteSpace(name))
+			{
+				var claim = new Claim(ClaimTypes.NameIdentifier, name);
+				payload.AddClaim(claim);
+			}
+
+			var secToken = new JwtSecurityToken(_header, payload);
+			var handler = new JwtSecurityTokenHandler();
+
+			return handler.WriteToken(secToken);
+		}
+	}
+
+	[Flags]
+	public enum Roles : byte
+	{
+		Admin = 1,
+		Customer = 2,
+		User = 4,
+	}
+}
