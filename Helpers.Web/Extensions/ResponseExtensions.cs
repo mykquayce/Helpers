70dbf83a83013c791b4f224eca920a4f8a70b3c6
Bob <myk_quayce@hotmail.com>
2020-10-18 17:10:13 +0100

    iresponse tuple deconstruction
    ...and some nuget update

---
+++ Helpers.Web/Extensions/ResponseExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Helpers.Web.Models;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Helpers.Web.Extensions
+{
+	public static class ResponseExtensions
+	{
+		public static void Deconstruct(
+			this IResponse response,
+			out IReadOnlyDictionary<string, StringValues>? headers, out HttpStatusCode? statusCode, out Task<Stream>? taskStream)
+		{
+			if (response is null) throw new ArgumentNullException(nameof(response));
+
+			headers = response.Headers;
+			statusCode = response.StatusCode;
+			taskStream = response.TaskStream;
+		}
+		public static void Deconstruct<T>(
+			this IResponse<T> response,
+			out IReadOnlyDictionary<string, StringValues>? headers, out HttpStatusCode? statusCode, out Task<Stream>? taskStream, out T? @object)
+			where T : class
+		{
+			if (response is null) throw new ArgumentNullException(nameof(response));
+
+			headers = response.Headers;
+			statusCode = response.StatusCode;
+			taskStream = response.TaskStream;
+			@object = response.Object;
+		}
+	}
+}
