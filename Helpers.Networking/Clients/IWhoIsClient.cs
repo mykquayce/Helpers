6f816dd7b031ffb072a5ea15636b23893dce9acb
Bob H <myk_quayce@hotmail.com>
2021-02-16 17:59:07 +0000

    making WhoIsClient disposable
    ...because it extends TcpClient, which is disposable

--- Helpers.Networking/Clients/IWhoIsClient.cs
+++ Helpers.Networking/Clients/IWhoIsClient.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace Helpers.Networking.Clients
 {
-	public interface IWhoIsClient
+	public interface IWhoIsClient : IDisposable
 	{
 		IAsyncEnumerable<Models.SubnetAddress> GetIpsAsync(int asn);
 	}
