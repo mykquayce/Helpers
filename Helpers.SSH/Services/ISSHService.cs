defee3fd052179a1188356ea9aad729b69b761dd
Bob H <bob@codes.com>
2021-03-07 16:44:09 +0000

    ssh helpers

---
+++ Helpers.SSH/Services/ISSHService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Helpers.SSH.Services
+{
+	public interface ISSHService : IDisposable
+	{
+		Task<string> RunCommandAsync(string commandText, int millisecondsTimeout = 5_000);
+		IAsyncEnumerable<Helpers.Networking.Models.SubnetAddress> GetBlackholesAsync();
+		Task AddBlackholeAsync(Helpers.Networking.Models.SubnetAddress subnetAddress);
+		Task AddBlackholesAsync(IEnumerable<Helpers.Networking.Models.SubnetAddress> subnetAddresses);
+		Task DeleteBlackholeAsync(Helpers.Networking.Models.SubnetAddress subnetAddress);
+		Task DeleteBlackholesAsec(IEnumerable<Helpers.Networking.Models.SubnetAddress> subnetAddresses);
+	}
+}
