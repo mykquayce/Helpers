d16596a49f3d36a10d07703e35eb9031226eb2d7
Bob H <mail@bob.codes>
2021-03-23 16:31:21 +0000

    tests

--- Helpers.GlobalCache.Tests/ServiceTests.cs
+++ Helpers.GlobalCache.Tests/ServiceTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Xunit;
 
 namespace Helpers.GlobalCache.Tests
@@ -17,48 +14,35 @@ namespace Helpers.GlobalCache.Tests
 		}
 
 		[Theory]
-		[InlineData("000c1e059cad")]
-		public async Task Discover(string expected)
+		[InlineData("iTach059CAD", "sendir,1:1,3,40192,1,1,96,24,48,24,24,24,48,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,4000\r", "completeir,1:1,3\r")]
+		public async Task Power(string hostName, string message, string expected)
 		{
-			var actual = await _sut.DiscoverAsync().FirstAsync();
-
-			Assert.Equal(expected, actual.ToString(), StringComparer.InvariantCultureIgnoreCase);
+			var response = await _sut.ConnectSendReceiveAsync(hostName, message);
+			Assert.Equal(expected, response);
 		}
 
 		[Theory]
-		[InlineData("000c1e059cad", "sendir,1:1,4,40192,1,1,96,24,48,24,24,24,48,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,4000\r")]
-		public async Task Power(string endPointString, string message)
+		[InlineData("iTach059CAD", "sendir,1:1,3,40192,1,1,96,24,24,24,48,24,24,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,4000\r", "completeir,1:1,3\r")]
+		public async Task VolumeUp(string hostName, string message, string expected)
 		{
-			await Discover(endPointString);
-			var endPoint = PhysicalAddress.Parse(endPointString);
-			await _sut.SendMessageasync(endPoint, message);
+			var response = await _sut.ConnectSendReceiveAsync(hostName, message);
+			Assert.Equal(expected, response);
 		}
 
 		[Theory]
-		[InlineData("000c1e059cad", "sendir,1:1,3,40192,1,1,96,24,24,24,48,24,24,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,4000\r")]
-		public async Task VolumeUp(string endPointString, string message)
+		[InlineData("iTach059CAD", "sendir,1:1,3,40192,1,1,96,24,48,24,48,24,24,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,4000\r", "completeir,1:1,3\r")]
+		public async Task VolumeDown(string hostName, string message, string expected)
 		{
-			await Discover(endPointString);
-			var endPoint = PhysicalAddress.Parse(endPointString);
-			await _sut.SendMessageasync(endPoint, message);
+			var response = await _sut.ConnectSendReceiveAsync(hostName, message);
+			Assert.Equal(expected, response);
 		}
 
 		[Theory]
-		[InlineData("000c1e059cad", "sendir,1:1,4,40192,1,1,96,24,48,24,48,24,24,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,4000\r")]
-		public async Task VolumeDown(string endPointString, string message)
+		[InlineData("iTach059CAD", "sendir,1:1,3,40064,1,1,96,24,24,24,24,24,48,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,897\r", "completeir,1:1,3\r")]
+		public async Task Mute(string hostName, string message, string expected)
 		{
-			await Discover(endPointString);
-			var endPoint = PhysicalAddress.Parse(endPointString);
-			await _sut.SendMessageasync(endPoint, message);
-		}
-
-		[Theory]
-		[InlineData("000c1e059cad", "sendir,1:1,1,40064,1,1,96,24,24,24,24,24,48,24,24,24,48,24,24,24,24,24,24,24,24,24,24,24,24,24,48,24,48,24,24,24,24,897\r")]
-		public async Task Mute(string endPointString, string message)
-		{
-			await Discover(endPointString);
-			var endPoint = PhysicalAddress.Parse(endPointString);
-			await _sut.SendMessageasync(endPoint, message);
+			var response = await _sut.ConnectSendReceiveAsync(hostName, message);
+			Assert.Equal(expected, response);
 		}
 	}
 }
