9f8a5062c9fed234fc53a48d437a8846e456f951
Bob H <mail@bob.codes>
2021-03-23 13:53:25 +0000

    ping tests

--- Helpers.Networking.Tests/NetworkHelpersTests.cs
+++ Helpers.Networking.Tests/NetworkHelpersTests.cs
@@ -11,11 +11,21 @@ namespace Helpers.Networking.Tests
 
 		[Theory]
 		[InlineData("192.168.1.50", IPStatus.Success)]
-		public async Task Ping(string ipAddressString, IPStatus expected)
+		public async Task Ping_IPAddress(string ipAddressString, IPStatus expected)
 		{
 			var ipAddress = IPAddress.Parse(ipAddressString);
 			var status = await NetworkHelpers.PingAsync(ipAddress);
 			Assert.Equal(expected, status);
+		}
+
+		[Theory]
+		[InlineData("iTach059CAD", @"^192\.168\.1\.\d+$", IPStatus.Success)]
+		public async Task Ping_HostName(string hostName, string expectedIPAddressPattern, IPStatus expectedStatus)
+		{
+			var (actualIPAddress, actualStatus) = await NetworkHelpers.PingAsync(hostName);
+			Assert.NotNull(actualIPAddress);
+			Assert.Matches(expectedIPAddressPattern, actualIPAddress.ToString());
+			Assert.Equal(expectedStatus, actualStatus);
 		}
 
 		[Theory]
