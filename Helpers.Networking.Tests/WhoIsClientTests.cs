18b2290d908b06679a608bb777dca94072c5d771
Bob H <mail@bob.codes>
2021-04-06 18:32:53 +0100

    adding some WhoIsClient tests

--- Helpers.Networking.Tests/WhoIsClientTests.cs
+++ Helpers.Networking.Tests/WhoIsClientTests.cs
@@ -1,4 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -27,6 +33,123 @@ namespace Helpers.Networking.Tests
 				Assert.NotEmpty(ip.ToString());
 				Assert.InRange(mask ?? -1, 0, 64);
 			}
+		}
+
+		[Theory]
+		[InlineData("riswhois.ripe.net", 43, "-F -K -i 32934\n")]
+		public async Task GetAsn(string host, int port, string command)
+		{
+			var encoding = Encoding.ASCII;
+			using var client = new TcpClient();
+			await client.ConnectAsync(host, port);
+			await using var stream = client.GetStream();
+			{
+				using var writer = new StreamWriter(stream, encoding, leaveOpen: true)
+				{
+					NewLine = "\n",
+				};
+				await writer.WriteLineAsync(command);
+				await writer.FlushAsync();
+			}
+
+			Assert.True(stream.CanRead);
+
+			string? response;
+			{
+				using var reader = new StreamReader(stream, encoding);
+				response = await reader.ReadToEndAsync();
+			}
+
+			Assert.False(stream.CanRead);
+			Assert.NotNull(response);
+			Assert.NotEmpty(response);
+		}
+
+		[Theory]
+		[InlineData(
+			"% This is RIPE NCC's Routing Information Service\n% whois gateway to collected BGP Routing Tables, version2.0\n% IPv4 or IPv6 address to origin prefix match\n%\n% For more information visit http://www.ripe.net/ris/riswhois.html\n%\n% Connected to backend ris-whois12.ripe.net\n\n16509\t13.224.220.0/22\n\n\n",
+			"16509\t13.224.220.0/22")]
+		public void Parse(string input, params string[] expected)
+		{
+			var actual = input
+				.Split(new char[2] { '\r', '\n', }, StringSplitOptions.RemoveEmptyEntries)
+				.Where(s => !s.StartsWith('%'))
+				.ToList();
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Theory]
+		[InlineData("facebook")]
+		[InlineData("instagram")]
+		//[InlineData("oculus")]//, "31.13.72.51")]
+		[InlineData("whatsapp")]
+		public async Task IpFromWebsite(string org)
+		{
+			var addresses = Dns.GetHostAddresses($"www.{org}.com");
+
+			Assert.NotNull(addresses);
+			Assert.NotEmpty(addresses);
+		}
+
+		[Theory]
+		[InlineData("31.13.72.174")]
+		public async Task IpLookup(string ipString)
+		{
+			var ip = IPAddress.Parse(ipString);
+			var command = $"-L {ip}\n";
+			var tcpClient = new Clients.Concrete.TcpClient("riswhois.ripe.net", 43);
+			var reply = await tcpClient.SendAndReceiveAsync(command);
+			Assert.NotNull(reply);
+			Assert.NotEmpty(reply);
+		}
+
+		[Theory]
+		[InlineData(
+			"% This is RIPE NCC's Routing Information Service\n% whois gateway to collected BGP Routing Tables, version2.0\n% IPv4 or IPv6 address to origin prefix match\n%\n% For more information visit http://www.ripe.net/ris/riswhois.html\n%\n% Connected to backend ris-whois11.ripe.net\n\nroute:        31.13.64.0/18\norigin:       AS32934\ndescr:        FACEBOOK, US\nlastupd-frst: 2021-03-19 09:04Z  91.206.52.68@rrc20\nlastupd-last: 2021-04-02 23:55Z  37.49.237.239@rrc21\nseen-at:      rrc00,rrc01,rrc03,rrc04,rrc05,rrc06,rrc07,rrc10,rrc11,rrc12,rrc13,rrc14,rrc15,rrc16,rrc18,rrc19,rrc20,rrc21,rrc22,rrc23,rrc24,rrc25\nnum-rispeers: 386\nsource:       RISWHOIS\n\n\n",
+			"route", "origin", "descr", "lastupd-frst", "lastupd-last", "seen-at", "num-rispeers", "source")]
+		/*[InlineData(
+			"% This is RIPE NCC's Routing Information Service\n% whois gateway to collected BGP Routing Tables, version2.0\n% IPv4 or IPv6 address to origin prefix match\n%\n% For more information visit http://www.ripe.net/ris/riswhois.html\n%\n% Connected to backend ris-whois11.ripe.net\n\nroute:        31.13.64.0/18\norigin:       AS32934\ndescr:        FACEBOOK, US\nlastupd-frst: 2021-03-19 09:04Z  91.206.52.68@rrc20\nlastupd-last: 2021-04-02 23:55Z  37.49.237.239@rrc21\nseen-at:      rrc00,rrc01,rrc03,rrc04,rrc05,rrc06,rrc07,rrc10,rrc11,rrc12,rrc13,rrc14,rrc15,rrc16,rrc18,rrc19,rrc20,rrc21,rrc22,rrc23,rrc24,rrc25\nnum-rispeers: 386\nsource:       RISWHOIS\n\nroute:        31.13.72.0/24\norigin:       AS32934\ndescr:        FACEBOOK, US\nlastupd-frst: 2021-03-19 09:05Z  91.206.52.116@rrc20\nlastupd-last: 2021-04-02 18:27Z  80.81.192.231@rrc12\nseen-at:      rrc00,rrc01,rrc03,rrc04,rrc05,rrc06,rrc07,rrc10,rrc11,rrc12,rrc13,rrc14,rrc15,rrc16,rrc18,rrc19,rrc20,rrc21,rrc22,rrc23,rrc24,rrc25\nnum-rispeers: 332\nsource:       RISWHOIS\n\n\n",
+			"route", "origin", "descr", "lastupd-frst", "lastupd-last", "seen-at", "num-rispeers", "source")]*/
+		public void DeserializeToDictionary(string input, params string[] keys)
+		{
+			var dictionary = input
+				.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+				.Where(s => !s.StartsWith('%'))
+				.Select(line => Split(line, 14))
+				.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.InvariantCultureIgnoreCase);
+
+			Assert.NotNull(dictionary);
+			Assert.NotEmpty(dictionary);
+
+			foreach (var key in keys)
+			{
+				Assert.Contains(key, dictionary.Keys);
+				var value = dictionary[key];
+				Assert.NotNull(value);
+				Assert.NotEmpty(value);
+				Assert.Matches(@"^\w", value);
+				Assert.Matches(@"\w$", value);
+			}
+		}
+
+		[Theory]
+		[InlineData("route:        31.13.64.0/18", "route", "31.13.64.0/18")]
+		[InlineData("origin:       AS32934", "origin", "AS32934")]
+		[InlineData("descr:        FACEBOOK, US", "descr", "FACEBOOK, US")]
+		public void SplitTests(string line, string expectedKey, string expectedValue)
+		{
+			var (key, value) = Split(line, 14);
+			Assert.Equal(expectedKey, key);
+			Assert.Equal(expectedValue, value);
+		}
+
+		public static KeyValuePair<string, string> Split(string s, int column)
+		{
+			var key = s[..column].TrimEnd(' ')[..^1];
+			var value = s[column..];
+			var kvp = new KeyValuePair<string, string>(key, value);
+			return kvp;
 		}
 	}
 }
