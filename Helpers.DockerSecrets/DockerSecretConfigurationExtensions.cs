82028792e0f5f395af73df453777a564116d03b8
Bob H <mail@bob.codes>
2021-04-27 19:04:09 +0100

    updating docker secrets extensions with filenameCharsToSwapWithColons

--- Helpers.DockerSecrets/DockerSecretConfigurationExtensions.cs
+++ Helpers.DockerSecrets/DockerSecretConfigurationExtensions.cs
@@ -2,12 +2,16 @@
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.FileProviders.Physical;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.Extensions.Configuration
 {
 	public static class DockerSecretConfigurationExtensions
 	{
-		private static readonly string _path = Path.Combine(Path.DirectorySeparatorChar.ToString(), "run", "secrets");
+		private static readonly string _path = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+			? Path.Combine("C:", "ProgramData", "Docker", "secrets")
+			: Path.Combine(Path.DirectorySeparatorChar.ToString(), "run", "secrets");
+
 		private static readonly IFileProvider _fileProvider = new PhysicalFileProvider(_path, ExclusionFilters.Sensitive);
 
 		/// <summary>
@@ -16,11 +20,13 @@ namespace Microsoft.Extensions.Configuration
 		/// <param name="configurationBuilder"></param>
 		/// <param name="optional"></param>
 		/// <param name="reloadOnChange"></param>
+		/// <param name="filenameCharsToSwapWithColons">case sensitive</param>
 		/// <returns></returns>
 		public static IConfigurationBuilder AddDockerSecrets(
 			this IConfigurationBuilder configurationBuilder,
 			bool optional = false,
-			bool reloadOnChange = false)
+			bool reloadOnChange = false,
+			params char[] filenameCharsToSwapWithColons)
 		{
 			Guard.Argument(() => configurationBuilder).NotNull();
 
@@ -29,6 +35,11 @@ namespace Microsoft.Extensions.Configuration
 			foreach (var file in files)
 			{
 				var configKey = file.Name;
+
+				foreach (var c in filenameCharsToSwapWithColons)
+				{
+					configKey = configKey.Replace(c, ':');
+				}
 
 				var source = new DockerSecretConfigurationSource(configKey)
 				{
