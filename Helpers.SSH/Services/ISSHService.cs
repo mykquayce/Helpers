7a15e36b32c7c440db465066abb55efb0d1cb3e9
Bob H <mail@bob.codes>
2021-05-11 23:34:19 +0100

    Helpers.SSH ...

--- Helpers.SSH/Services/ISSHService.cs
+++ Helpers.SSH/Services/ISSHService.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace Helpers.SSH.Services
 {
 	public interface ISSHService : IDisposable
 	{
-		IAsyncEnumerable<Helpers.Networking.Models.SubnetAddress> GetBlackholesAsync();
-		Task AddBlackholeAsync(Helpers.Networking.Models.SubnetAddress subnetAddress);
-		Task AddBlackholesAsync(IEnumerable<Helpers.Networking.Models.SubnetAddress> subnetAddresses);
-		Task DeleteBlackholeAsync(Helpers.Networking.Models.SubnetAddress subnetAddress);
-		Task DeleteBlackholesAsync(IEnumerable<Helpers.Networking.Models.SubnetAddress> subnetAddresses);
+		IAsyncEnumerable<Helpers.Networking.Models.AddressPrefix> GetBlackholesAsync();
+		Task AddBlackholeAsync(Helpers.Networking.Models.AddressPrefix subnetAddress);
+		Task AddBlackholesAsync(IEnumerable<Helpers.Networking.Models.AddressPrefix> subnetAddresses);
+		Task DeleteBlackholeAsync(Helpers.Networking.Models.AddressPrefix subnetAddress);
+		Task DeleteBlackholesAsync(IEnumerable<Helpers.Networking.Models.AddressPrefix> subnetAddresses);
 		Task DeleteBlackholesAsync();
-		IAsyncEnumerable<Helpers.Networking.Models.DhcpEntry> GetDhcpLeasesAsync();
+		IAsyncEnumerable<Helpers.Networking.Models.DhcpLease> GetDhcpLeasesAsync();
+		Task<Helpers.Networking.Models.DhcpLease> GetLeaseByIPAddressAsync(IPAddress ipAddress);
+		Task<Helpers.Networking.Models.DhcpLease> GetLeaseByPhysicalAddressAsync(PhysicalAddress physicalAddress);
 	}
 }
