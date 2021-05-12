5aac5353ebc37b90a272e217e10f60e99eae3a96
Bob H <mail@bob.codes>
2021-05-12 12:13:39 +0100

    Helpers.Elgato

---
+++ Helpers.Elgato.Tests/CachingServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using Xunit;
+
+namespace Helpers.Elgato.Tests
+{
+	public class CachingServiceTests
+	{
+		[Fact]
+		public void Tests()
+		{
+			var sut = new Concrete.AddressesCache();
+
+			sut.Set(PhysicalAddress.Parse("1800db0e980d"), IPAddress.Parse("192.168.1.180"), DateTimeOffset.MaxValue);
+			sut.Set(PhysicalAddress.Parse("ccf735b2a678"), IPAddress.Parse("192.168.1.179"), DateTimeOffset.MaxValue);
+			sut.Set(PhysicalAddress.Parse("000c1e059cad"), IPAddress.Parse("192.168.1.121"), DateTimeOffset.MaxValue);
+
+			bool ok;
+			IPAddress? actual;
+
+			ok = sut.TryGet(PhysicalAddress.Parse("1800db0e980d"), out actual);
+
+			Assert.True(ok);
+			Assert.NotNull(actual);
+			Assert.Equal("192.168.1.180", actual!.ToString());
+
+			ok = sut.TryGet(PhysicalAddress.Parse("ccf735b2a678"), out actual);
+
+			Assert.True(ok);
+			Assert.NotNull(actual);
+			Assert.Equal("192.168.1.179", actual!.ToString());
+
+			ok = sut.TryGet(PhysicalAddress.Parse("000c1e059cad"), out actual);
+
+			Assert.True(ok);
+			Assert.NotNull(actual);
+			Assert.Equal("192.168.1.121", actual!.ToString());
+		}
+
+		[Fact]
+		public void Dictionary()
+		{
+			IDictionary<PhysicalAddress, IPAddress> sut = new Dictionary<PhysicalAddress, IPAddress>()
+			{
+				[PhysicalAddress.Parse("1800db0e980d")] = IPAddress.Parse("192.168.1.180"),
+				[PhysicalAddress.Parse("ccf735b2a678")] = IPAddress.Parse("192.168.1.179"),
+				[PhysicalAddress.Parse("000c1e059cad")] = IPAddress.Parse("192.168.1.121"),
+			};
+
+			Assert.Equal("192.168.1.180", sut[PhysicalAddress.Parse("1800db0e980d")].ToString());
+			Assert.Equal("192.168.1.179", sut[PhysicalAddress.Parse("ccf735b2a678")].ToString());
+			Assert.Equal("192.168.1.121", sut[PhysicalAddress.Parse("000c1e059cad")].ToString());
+		}
+	}
+}
---
+++ Helpers.Elgato/Concrete/ElgatoService.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace Helpers.Elgato.Concrete
+{
+	public sealed class ElgatoService : IElgatoService
+	{
+		private const int _port = 9_123;
+		private readonly Helpers.SSH.Services.ISSHService _sshService;
+		private readonly IElgatoClient _client = new ElgatoClient();
+		private readonly static IAddressesCache _cache = new AddressesCache();
+
+		public ElgatoService(Helpers.SSH.Services.ISSHService sshService)
+		{
+			_sshService = sshService;
+		}
+
+		public void Dispose() => _client?.Dispose();
+
+		public async Task<IPAddress> GetIPAddressAsync(PhysicalAddress physicalAddress)
+		{
+			if (_cache.TryGet(physicalAddress, out var ipAddress))
+			{
+				return ipAddress!;
+			}
+
+			var lease = await _sshService.GetLeaseByPhysicalAddressAsync(physicalAddress);
+			_cache.Set(physicalAddress, lease.IPAddress, lease.Expiration.AddMinutes(-5));
+
+			return lease.IPAddress;
+		}
+
+		public async Task<Models.AccessoryInfoObject> GetAccessoryInfoAsync(PhysicalAddress physicalAddress)
+		{
+			var ipAddress = await GetIPAddressAsync(physicalAddress);
+			var endPoint = new IPEndPoint(ipAddress!, _port);
+			return await _client.GetAccessoryInfoAsync(endPoint);
+		}
+
+		public async Task<Models.MessageObject.LightObject> GetLightAsync(PhysicalAddress physicalAddress)
+		{
+			var ipAddress = await GetIPAddressAsync(physicalAddress);
+			var endPoint = new IPEndPoint(ipAddress!, _port);
+			return await _client.GetLightAsync(endPoint);
+		}
+
+		public async Task SetLightAsync(PhysicalAddress physicalAddress, Models.MessageObject.LightObject light)
+		{
+			var ipAddress = await GetIPAddressAsync(physicalAddress);
+			var endPoint = new IPEndPoint(ipAddress!, _port);
+			await _client.SetLightAsync(endPoint, light);
+		}
+
+		public async Task ToggleLightPowerStateAsync(PhysicalAddress physicalAddress)
+		{
+			var ipAddress = await GetIPAddressAsync(physicalAddress);
+			var endPoint = new IPEndPoint(ipAddress!, _port);
+
+			var old = await _client.GetLightAsync(endPoint);
+
+			var @new = old with { on = old.on == 1 ? (byte)0 : (byte)1, };
+
+			await _client.SetLightAsync(endPoint, @new);
+		}
+	}
+}
