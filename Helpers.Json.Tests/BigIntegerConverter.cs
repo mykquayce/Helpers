4ba9c779a90000c88a9a0961bbd3320a5a6485dc
Bob H <mail@bob.codes>
2021-05-13 19:41:00 +0100

    Helpers.Json : Uri and BigInteger converters

---
+++ Helpers.Json.Tests/BigIntegerConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Helpers.Json.Tests
+{
+	public class BigIntegerConverter
+	{
+		private record Record([property: JsonConverter(typeof(Converters.JsonBigIntegerConverter))] BigInteger BigInteger);
+
+		[Theory]
+		[InlineData(@"{""BigInteger"":""-234702348570928347509283475023450987092834""}")]
+		[InlineData(@"{""BigInteger"":""-1""}")]
+		[InlineData(@"{""BigInteger"":""1""}")]
+		[InlineData(@"{""BigInteger"":""234702348570928347509283475023450987092834""}")]
+		public void Test(string json)
+		{
+			var record = JsonSerializer.Deserialize<Record>(json);
+
+			Assert.NotNull(record);
+			Assert.NotEqual(0, record!.BigInteger);
+
+			var jsonAgain = JsonSerializer.Serialize(record);
+
+			Assert.Equal(json, jsonAgain);
+		}
+	}
+}
---
+++ Helpers.Json/Converrters/JsonUriConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Helpers.Json.Converters
+{
+	public class JsonUriConverter : JsonConverter<Uri>
+	{
+		public override Uri? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			return Uri.TryCreate(reader.GetString(), UriKind.RelativeOrAbsolute, out var result)
+				? result
+				: default;
+		}
+
+		public override void Write(Utf8JsonWriter writer, Uri value, JsonSerializerOptions options)
+		{
+			writer.WriteStringValue(value.ToString());
+		}
+	}
+}
