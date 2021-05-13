28255c7d47fee014142048f8226dbbd98316515d
Bob H <mail@bob.codes>
2021-05-13 02:26:13 +0100

    Helpers.Networking.Models : JSON serialisation support for DhcpLease

--- Helpers.Networking.Models/DhcpLease.cs
+++ Helpers.Networking.Models/DhcpLease.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Text.Json.Serialization;
 
 namespace Helpers.Networking.Models
 {
-	public record DhcpLease(DateTime Expiration, PhysicalAddress PhysicalAddress, IPAddress IPAddress, string? HostName, string? Identifier);
+	public record DhcpLease(
+		DateTime Expiration,
+		[property: JsonConverter(typeof(Helpers.Json.Converters.JsonPhysicalAddressConverter))] PhysicalAddress PhysicalAddress,
+		[property: JsonConverter(typeof(Helpers.Json.Converters.JsonIPAddressConverter))] IPAddress IPAddress,
+		string? HostName,
+		string? Identifier);
 }
