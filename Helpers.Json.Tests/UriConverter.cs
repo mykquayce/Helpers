c8784a27cfe7606d8431b73f8c16a8f13c592306
Bob H <mail@bob.codes>
2021-05-13 19:41:00 +0100

    Helpers.Json : Uri and BigInteger converters

---
+++ Helpers.Json.Tests/UriConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Helpers.Json.Tests
+{
+	public class UriConverter
+	{
+		private record Record([property: JsonConverter(typeof(Converters.JsonUriConverter))] Uri Uri);
+
+		[Theory]
+		[InlineData(@"{""Uri"":""https://old.reddit.com/""}")]
+		[InlineData(@"{""Uri"":""https://old.reddit.com:2342/""}")]
+		[InlineData(@"{""Uri"":""https://old.reddit.com/r/random""}")]
+		[InlineData(@"{""Uri"":""/r/random""}")]
+		[InlineData(@"{""Uri"":""/r/random?key1=value""}")]
+		[InlineData(@"{""Uri"":""/r/random?key1=value\u0026key2=value""}")]
+		public void Test(string json)
+		{
+			var record = JsonSerializer.Deserialize<Record>(json);
+
+			Assert.NotNull(record);
+			Assert.NotNull(record!.Uri);
+
+			var jsonAgain = JsonSerializer.Serialize(record);
+
+			Assert.Equal(json, jsonAgain);
+		}
+	}
+}
---
+++ Helpers.Json/Converrters/JsonBigIntegerConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Helpers.Json.Converters
+{
+	public class JsonBigIntegerConverter : JsonConverter<BigInteger>
+	{
+		public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			return BigInteger.TryParse(reader.GetString(), out var result)
+				? result
+				: default;
+		}
+
+		public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
+		{
+			writer.WriteStringValue(value.ToString());
+		}
+	}
+}
