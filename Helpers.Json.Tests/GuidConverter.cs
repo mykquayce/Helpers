5de5dc6f8ac49b7e029d5b4149bc939706549c23
Bob H <mail@bob.codes>
2021-05-28 14:10:40 +0100

    json guid converter

---
+++ Helpers.Json.Tests/GuidConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Helpers.Json.Tests
+{
+	public class GuidConverter
+	{
+		private record Record(
+			Guid? WithoutConverter,
+			[property: JsonConverter(typeof(Converters.JsonGuidConverter))] Guid WithConverter);
+
+		[Theory]
+		[InlineData(@"{""WithConverter"":""f31254bf-7b67-5e3a-6766-af5069453450""}", "f31254bf-7b67-5e3a-6766-af5069453450")]
+		[InlineData(@"{""WithConverter"":""F31254BF-7B67-5E3A-6766-AF5069453450""}", "f31254bf-7b67-5e3a-6766-af5069453450")]
+		[InlineData(@"{""WithConverter"":""f31254bf7b675e3a6766af5069453450""}", "f31254bf-7b67-5e3a-6766-af5069453450")]
+		[InlineData(@"{""WithConverter"":""F31254BF7B675E3A6766AF5069453450""}", "f31254bf-7b67-5e3a-6766-af5069453450")]
+		public void Test(string json, string expected)
+		{
+			var record = JsonSerializer.Deserialize<Record>(json);
+
+			Assert.NotNull(record);
+			Assert.NotEqual(default, record!.WithConverter);
+			Assert.Equal(Guid.Parse(expected), record.WithConverter);
+		}
+
+		[Theory]
+		[InlineData(@"{""WithoutConverter"":""f31254bf7b675e3a6766af5069453450""}")]
+		[InlineData(@"{""WithoutConverter"":""F31254BF7B675E3A6766AF5069453450""}")]
+		public void ProveNecessity(string json)
+		{
+			void action() => JsonSerializer.Deserialize<Record>(json);
+			Assert.Throws<JsonException>(action);
+		}
+	}
+}
