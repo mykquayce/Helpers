92b7647fe3298b52a6b9f2df1639488a5cfadbfe
bob h <mail@bob.codes>
2021-10-02 03:02:34 +0100

    helpers.networking : pingclient config extending ioptions

--- Helpers.Networking.Tests/Fixtures/PingClientFixture.cs
+++ Helpers.Networking.Tests/Fixtures/PingClientFixture.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.Extensions.Options;
-
-namespace Helpers.Networking.Tests.Fixtures
+﻿namespace Helpers.Networking.Tests.Fixtures
 {
 	public class PingClientFixture
 	{
 		public PingClientFixture()
 		{
 			var config = Clients.Concrete.PingClient.Config.Defaults;
-			var options = Options.Create(config);
-			PingClient = new Clients.Concrete.PingClient(options);
+			PingClient = new Clients.Concrete.PingClient(config);
 		}
 
 		public Clients.IPingClient PingClient { get; }
