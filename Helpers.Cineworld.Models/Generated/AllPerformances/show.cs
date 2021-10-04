d8e6f67156ead9180b06ae607ea1c11fff4d715b
bob h <mail@bob.codes>
2021-10-04 16:45:59 +0100

    helpers.cineworld.models : adding shows to all-performances

---
+++ Helpers.Cineworld.Models/Generated/AllPerformances/show.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Helpers.Cineworld.Models.Generated.AllPerformances;
+
+public partial class show
+{
+	private readonly static IFormatProvider _formatProvider = CultureInfo.InvariantCulture;
+	private const DateTimeStyles _dateTimeStyles = DateTimeStyles.AssumeLocal | DateTimeStyles.AdjustToUniversal;
+
+	public DateTime DateTime
+	{
+		get
+		{
+			var date = DateTime.ParseExact(this.date, "ddd dd MMM", _formatProvider, _dateTimeStyles);
+			var time = TimeSpan.ParseExact(this.time, "hh\\:mm", _formatProvider);
+			return date + time;
+		}
+	}
+}
