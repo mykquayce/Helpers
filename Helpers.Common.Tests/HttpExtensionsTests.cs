3c0514fa01148701a1b8e067264430e52b67825e
bob h <mail@bob.codes>
2021-10-11 17:49:50 +0100

    helpers.common : moving namespaces around

---
+++ Helpers.Common.Tests/HttpExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Helpers.Common.Tests;
+
+public class HttpExtensionsTests : IClassFixture<Fixtures.HttpClientFixture>
+{
+	private readonly HttpClient _httpClient;
+
+	public HttpExtensionsTests(Fixtures.HttpClientFixture fixture)
+	{
+		_httpClient = fixture.HttpClient;
+	}
+
+	[Theory]
+	[InlineData("https://www.httpbin.org/get")]
+	public async Task HttpBinResponse(string uriString)
+	{
+		// Arrange
+		var uri = new Uri(uriString);
+
+		// Act
+		var response = await _httpClient.GetAsync<HttpBinResponseObject>(uri);
+
+		// Assert
+		Assert.NotNull(response);
+		Assert.Equal(uriString, response.url);
+		Assert.NotNull(response.headers);
+		Assert.NotEmpty(response.headers!);
+	}
+}
+
+public class HttpBinResponseObject
+{
+#pragma warning disable IDE1006 // Naming Styles
+	public Dictionary<string, string>? args { get; set; }
+	public Dictionary<string, string>? headers { get; set; }
+	public string? origin { get; set; }
+	public string? url { get; set; }
+#pragma warning restore IDE1006 // Naming Styles
+}
--- Helpers.Common/DoubleEnumerator.cs
+++ Helpers.Common/DoubleEnumerator.cs
@@ -1,53 +1,49 @@
 ﻿using Dawn;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 
-namespace Helpers.Common
+namespace System.Collections.Generic;
+
+public class DoubleEnumerator<T> : IEnumerator<(T, T)>
 {
-	public class DoubleEnumerator<T> : IEnumerator<(T, T)>
+	private readonly IEnumerator<T> _first, _second;
+
+	public DoubleEnumerator(IEnumerator<T> first, IEnumerator<T> second)
 	{
-		private readonly IEnumerator<T> _first, _second;
-
-		public DoubleEnumerator(IEnumerator<T> first, IEnumerator<T> second)
-		{
-			_first = Guard.Argument(first).NotNull().Value;
-			_second = Guard.Argument(second).NotNull().Value;
-		}
-
-		public (T, T) Current => (_first.Current, _second.Current);
-
-		object IEnumerator.Current => Current;
-
-		public bool MoveNext() => _first.MoveNext() && _second.MoveNext();
-
-		public void Reset()
-		{
-			_first.Reset();
-			_second.Reset();
-		}
-
-		#region dispose
-		private bool _disposed;
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!_disposed)
-			{
-				if (disposing)
-				{
-					_first.Dispose();
-					_second.Dispose();
-				}
-
-				_disposed = true;
-			}
-		}
-
-		public void Dispose()
-		{
-			Dispose(disposing: true);
-			GC.SuppressFinalize(this);
-		}
-		#endregion dispose
+		_first = Guard.Argument(first).NotNull().Value;
+		_second = Guard.Argument(second).NotNull().Value;
 	}
+
+	public (T, T) Current => (_first.Current, _second.Current);
+
+	object IEnumerator.Current => Current;
+
+	public bool MoveNext() => _first.MoveNext() && _second.MoveNext();
+
+	public void Reset()
+	{
+		_first.Reset();
+		_second.Reset();
+	}
+
+	#region dispose
+	private bool _disposed;
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposed)
+		{
+			if (disposing)
+			{
+				_first.Dispose();
+				_second.Dispose();
+			}
+
+			_disposed = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+	#endregion dispose
 }
