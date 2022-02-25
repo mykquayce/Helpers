97429f7feb1ee5483c461f6fe763ac9523bd1492
bob h <mail@bob.codes>
2022-02-25 19:52:40 +0000

    helpers.common : IServiceCollection.FileConfigure
    ...allows config files to have values in files, e.g., Password_File = /run/secrets/password

---
+++ Helpers.Common.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace Helpers.Common.Tests;
+
+public class ServiceCollectionExtensionsTests
+{
+	[Theory]
+	[InlineData(default, default, default)]
+	[InlineData("alice", default, "alice")]
+	[InlineData(default, "./data/username.txt", "bob")]
+	[InlineData("alice", "./data/username.txt", "alice")]
+	public void BindObject_FromFile(string? username, string? usernameFile, string? expected)
+	{
+		// Arrange
+		IServiceProvider serviceProvider;
+		{
+			IConfiguration configuration;
+			{
+				var initialData = new Dictionary<string, string?>
+				{
+					["Username"] = username,
+					["Username_File"] = usernameFile,
+				};
+
+				configuration = new ConfigurationBuilder()
+					.AddInMemoryCollection(initialData)
+					.Build();
+			}
+
+			serviceProvider = new ServiceCollection()
+				.FileConfigure<Config>(configuration)
+				.BuildServiceProvider();
+
+			// Assert : old configuration hasn't changed
+			Assert.Equal(username, configuration["Username"]);
+		}
+
+		// Act
+		var options = serviceProvider.GetService<IOptions<Config>>();
+
+		// Assert
+		Assert.NotNull(options);
+		Assert.NotNull(options.Value);
+		Assert.Equal(expected, options.Value.Username);
+	}
+
+	public record Config(string? Username)
+	{
+		public Config() : this(default(string)) { }
+	}
+}
---
+++ Helpers.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+	private const BindingFlags _propertyFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.SetProperty;
+
+	public static IServiceCollection FileConfigure<TOptions>(this IServiceCollection services, IConfiguration configuration)
+		where TOptions : class
+	{
+		var type = typeof(TOptions);
+		var properties = type.GetProperties(_propertyFlags);
+
+		var additionals = new Dictionary<string, string>();
+
+		foreach (var property in from p in properties
+								 where p.PropertyType == typeof(string)
+								 where configuration[p.Name] is null
+								 select p)
+		{
+			var path = configuration[property.Name + "_File"];
+			if (string.IsNullOrWhiteSpace(path)) continue;
+			var contents = File.ReadAllText(path);
+			if (string.IsNullOrWhiteSpace(contents)) continue;
+			additionals.Add(property.Name, contents);
+		}
+
+		var augmentedConfiguration = new ConfigurationBuilder()
+			.AddConfiguration(configuration)
+			.AddInMemoryCollection(additionals)
+			.Build();
+
+		var t = augmentedConfiguration.Get<TOptions>();
+		var options = Options.Options.Create(t);
+		services.AddSingleton(options);
+
+		return services;
+	}
+}
