c9af62d6a05a27504f4f1cd78fda6b36e4890103
bob h <mail@bob.codes>
2022-03-05 21:15:20 +0000

    helpers.mysql : connect retry logic

--- Helpers.MySql/RepositoryBase.cs
+++ Helpers.MySql/RepositoryBase.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Dawn;
 using System.Data;
+using System.Net.Sockets;
 
 namespace Helpers.MySql;
 
@@ -20,7 +21,32 @@ public abstract class RepositoryBase
 			return;
 		}
 
-		_connection.Open();
+		var exceptions = new List<Exception>();
+		{
+			using var cts = new CancellationTokenSource(millisecondsDelay: 30_000);
+
+			while (_connection.State != ConnectionState.Open
+				&& !cts.IsCancellationRequested)
+			{
+				try
+				{
+					_connection.Open();
+				}
+				catch (AggregateException ex)
+					when (ex.Message == "One or more errors occurred. (Connection refused)"
+						&& ex.InnerExceptions.FirstOrDefault() is SocketException)
+				{
+					exceptions.Add(ex);
+					Thread.Sleep(millisecondsTimeout: 2_000);
+				}
+			}
+		}
+
+		if ((_connection.State & ConnectionState.Open) == 0)
+		{
+			var message = exceptions.Last().Message;
+			throw new AggregateException(message, exceptions);
+		}
 	}
 
 	protected Task<int> ExecuteAsync(string sql, object? param = default, IDbTransaction? transaction = default, int? commandTimeout = default, CommandType? commandType = default)
