449925a8396c1a2c1e1fe6e7b6011f002461c48f
bob h <mail@bob.codes>
2022-03-24 04:13:47 +0000

    fixing build script
    ...to use https

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-dotnet restore --source https://api.nuget.org/v3/index.json --source http://nuget/v3/index.json
+dotnet restore --source https://api.nuget.org/v3/index.json --source https://nuget/v3/index.json
 if [ $? -eq 1 ]; then exit 1; fi
 
 dotnet build --configuration Release --no-restore --verbosity minimal
@@ -11,5 +11,5 @@ if [ $? -eq 1 ]; then exit 1; fi
 
 if [ -n "$NuGetServerApiKey" ]
 then
-	dotnet nuget push ./nupkg/*.nupkg --api-key $NuGetServerApiKey --skip-duplicate --source http://nuget
+	dotnet nuget push ./nupkg/*.nupkg --api-key $NuGetServerApiKey --skip-duplicate --source https://nuget
 fi
