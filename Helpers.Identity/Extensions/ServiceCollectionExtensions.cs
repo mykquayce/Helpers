5ee9a2a656fb56d149ad1eaa5bbed24ab8a75c72
bob h <mail@bob.codes>
2022-03-27 14:15:38 +0100

    helpers.identity : IServiceCollection.AddAuthenticationAuthorization()

--- Helpers.Identity/Extensions/ServiceCollectionExtensions.cs
+++ Helpers.Identity/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Dawn;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -21,5 +22,40 @@ public static class ServiceCollectionExtensions
 			})
 			.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { AllowAutoRedirect = false, })
 			.Services;
+	}
+
+	public static IServiceCollection AddAuthenticationAuthorization(this IServiceCollection services, IConfiguration configuration)
+	{
+		var config = Helpers.Identity.Config.Defaults;
+		configuration.Bind(config);
+		return services.AddAuthenticationAuthorization(config);
+	}
+
+	public static IServiceCollection AddAuthenticationAuthorization(this IServiceCollection services, IOptions<Helpers.Identity.Config> options)
+	{
+		var config = Guard.Argument(options).NotNull().Wrap(o => o.Value)
+			.NotNull().Value;
+
+		services.AddAuthentication("Bearer")
+			.AddJwtBearer("Bearer", options =>
+			{
+				options.Authority = config.Authority.OriginalString;
+
+				options.TokenValidationParameters = new IdentityModel.Tokens.TokenValidationParameters
+				{
+					ValidateAudience = false,
+				};
+			});
+
+		services.AddAuthorization(options =>
+		{
+			options.AddPolicy("ApiScope", policy =>
+			{
+				policy.RequireAuthenticatedUser();
+				policy.RequireClaim(nameof(config.Scope), config.Scope);
+			});
+		});
+
+		return services;
 	}
 }
