48c111aaa6dcd49a8d0bc9e5afe55818a8d49929
mykquayce <myk_quayce@hotmail.com>
2022-03-29 02:59:13 +0100

    Identity (#70)
    * helpers.identity : IServiceCollection.AddAuthenticationAuthorization()
    
    * helpers.identity : nuget updates
    
    * helpers.identity : overload for IServiceCollection.AddIdentityClient
    
    * helpers.identity : version increment
    
    Co-authored-by: bob h <mail@bob.codes>

--- Helpers.Identity/Extensions/ServiceCollectionExtensions.cs
+++ Helpers.Identity/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Dawn;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -8,18 +9,60 @@ public static class ServiceCollectionExtensions
 {
 	public static IServiceCollection AddIdentityClient(this IServiceCollection services, IConfiguration configuration)
 	{
-		Guard.Argument(configuration).NotNull();
-		var authority = configuration[nameof(Helpers.Identity.Config.Authority)];
-		Guard.Argument(authority!).NotNull().NotEmpty().NotWhiteSpace();
-
+		var config = Helpers.Identity.Config.Defaults;
+		configuration.Bind(config);
 		return services
 			.Configure<Helpers.Identity.Config>(configuration)
+			.AddIdentityClient(config);
+	}
+
+	public static IServiceCollection AddIdentityClient(this IServiceCollection services, IOptions<Helpers.Identity.Config> options)
+	{
+		var config = Guard.Argument(options).NotNull().Wrap(o => o.Value)
+			.NotNull().Value;
+
+		return services
 			.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions()))
 			.AddHttpClient<Helpers.Identity.Clients.IIdentityClient, Helpers.Identity.Clients.Concrete.IdentityClient>(client =>
 			{
-				client.BaseAddress = new Uri(authority!, UriKind.Absolute);
+				client.BaseAddress = config.Authority;
 			})
 			.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { AllowAutoRedirect = false, })
 			.Services;
+	}
+
+	public static IServiceCollection AddAuthenticationAuthorization(this IServiceCollection services, IConfiguration configuration)
+	{
+		var config = Helpers.Identity.Config.Defaults;
+		configuration.Bind(config);
+		return services.AddAuthenticationAuthorization(config);
+	}
+
+	public static IServiceCollection AddAuthenticationAuthorization(this IServiceCollection services, IOptions<Helpers.Identity.Config> options)
+	{
+		var config = Guard.Argument(options).NotNull().Wrap(o => o.Value)
+			.NotNull().Value;
+
+		services.AddAuthentication("Bearer")
+			.AddJwtBearer("Bearer", options =>
+			{
+				options.Authority = config.Authority.OriginalString;
+
+				options.TokenValidationParameters = new IdentityModel.Tokens.TokenValidationParameters
+				{
+					ValidateAudience = false,
+				};
+			});
+
+		services.AddAuthorization(options =>
+		{
+			options.AddPolicy("ApiScope", policy =>
+			{
+				policy.RequireAuthenticatedUser();
+				policy.RequireClaim(nameof(config.Scope), config.Scope);
+			});
+		});
+
+		return services;
 	}
 }
