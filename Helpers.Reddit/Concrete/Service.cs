5674a4ef7659ea5d103e43b1b5adfefb6279ef28
bob h <mail@bob.codes>
2022-08-05 12:01:32 +0100

    removing link-related code from the service

--- Helpers.Reddit/Concrete/Service.cs
+++ Helpers.Reddit/Concrete/Service.cs
@@ -4,27 +4,23 @@ using System.Text.RegularExpressions;
 
 namespace Helpers.Reddit.Concrete;
 
-public class Service : IService
+public partial class Service : IService
 {
-	private static readonly Regex _threadRegex = new(@"^https:\/\/old\.reddit\.com\/r\/(\w+)\/comments\/(\w+)\/(\w+)\/$");
-	private static readonly Regex _linkRegex = new(@"\""(https?:\/\/.+?)\""");
+	private static readonly Regex _threadRegex = ThreadRegex();
+	private static readonly Regex _linkRegex = LinkRegex();
 	private readonly IClient _client;
-	private readonly IReadOnlyCollection<string> _denylist;
 
-	public Service(IClient client, IOptions<List<string>> denylistOptions)
+	public Service(IClient client)
 	{
 		_client = Guard.Argument(client).NotNull().Value;
-		_denylist = Guard.Argument(denylistOptions).NotNull().Wrap(o => o.Value)
-			.NotNull().NotEmpty().DoesNotContainNull().DoesNotContainDuplicate()
-			.Value;
 	}
 
 	public Task<string> GetRandomSubredditNameAsync(CancellationToken? cancellationToken = default)
-		=> _client.GetRandomSubredditNameAsync(cancellationToken);
+		=> _client.GetRandomSubredditAsync(cancellationToken);
 
 	public async IAsyncEnumerable<string> GetThreadIdsForSubredditAsync(string subredditName, CancellationToken? cancellationToken = default)
 	{
-		var threads = _client.GetThreadsFromSubredditAsync(subredditName, cancellationToken);
+		var threads = _client.GetThreadsAsync(subredditName, cancellationToken);
 
 		await using var enumerator = threads.GetAsyncEnumerator(cancellationToken ?? CancellationToken.None);
 
@@ -39,22 +35,11 @@ public class Service : IService
 	}
 
 	public IAsyncEnumerable<string> GetCommentsForThreadIdAsync(string subredditName, string threadId, CancellationToken? cancellationToken = default)
-		=> _client.GetCommentsFromThreadAsync(subredditName, threadId, cancellationToken);
+		=> _client.GetCommentsAsync(subredditName, threadId, cancellationToken);
 
-	public IEnumerable<Uri> GetLinksFromComment(string comment, CancellationToken? cancellationToken = default)
-	{
-		Guard.Argument(comment).NotNull().NotEmpty().NotWhiteSpace();
+	[RegexGenerator("^https:\\/\\/old\\.reddit\\.com\\/r\\/(\\w+)\\/comments\\/(\\w+)\\/(\\w+)\\/$")]
+	private static partial Regex ThreadRegex();
 
-		var matches = _linkRegex.Matches(comment);
-		var enumerator = matches.GetEnumerator();
-
-		while (cancellationToken?.IsCancellationRequested != true
-			&& enumerator.MoveNext())
-		{
-			var uriString = ((Match)enumerator.Current).Groups[1].Value;
-			if (!Uri.TryCreate(uriString, UriKind.Absolute, out var uri)) continue;
-			if (_denylist.Any(s => uri.Host.EndsWith(s, StringComparison.OrdinalIgnoreCase))) continue;
-			yield return uri;
-		}
-	}
+	[RegexGenerator("\\\"(https?:\\/\\/.+?)\\\"")]
+	private static partial Regex LinkRegex();
 }
--- Helpers.Reddit/IService.cs
+++ Helpers.Reddit/IService.cs
@@ -3,22 +3,6 @@
 public interface IService
 {
 	IAsyncEnumerable<string> GetCommentsForThreadIdAsync(string subredditName, string threadId, CancellationToken? cancellationToken = default);
-	IEnumerable<Uri> GetLinksFromComment(string comment, CancellationToken? cancellationToken = default);
 	Task<string> GetRandomSubredditNameAsync(CancellationToken? cancellationToken = default);
 	IAsyncEnumerable<string> GetThreadIdsForSubredditAsync(string subredditName, CancellationToken? cancellationToken = default);
-
-	async IAsyncEnumerable<Uri> GetLinksForThreadIdAsync(string subredditName, string threadId, CancellationToken? cancellationToken = default)
-	{
-		var comments = GetCommentsForThreadIdAsync(subredditName, threadId, cancellationToken);
-
-		await foreach (var comment in comments)
-		{
-			var links = GetLinksFromComment(comment, cancellationToken);
-
-			foreach (var link in links)
-			{
-				yield return link;
-			}
-		}
-	}
 }
