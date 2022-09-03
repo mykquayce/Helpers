274c3b642e84f8e590d658f9099d3460defbaba2
bob h <mail@bob.codes>
2022-09-03 13:25:38 +0100

    updating build process
    ...now runs in a container

--- build.ps1
+++ build.ps1
@@ -1,16 +1,10 @@
-dotnet restore --source https://api.nuget.org/v3/index.json --source https://nuget/v3/index.json `
+docker pull mcr.microsoft.com/dotnet/sdk:7.0 `
 	|| exit 1
 
+docker build `
+	--build-arg "NuGetServerApiKey=${env:NuGetServerApiKey}" `
+	--no-cache `
+	--secret id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt `
+	.
 
-dotnet build --configuration Release --no-restore --verbosity minimal `
-	|| exit 1
-
-
-dotnet pack --configuration Release --no-build --no-restore --nologo --output .\nupkg --runtime linux-x64 --verbosity minimal `
-	|| exit 1
-
-
-if (${env:NuGetServerApiKey} -ne $null) {
-	dotnet nuget push .\nupkg\*.nupkg --api-key ${env:NuGetServerApiKey} --skip-duplicate --source https://nuget `
-		|| exit 1
-}
+docker image prune --force
