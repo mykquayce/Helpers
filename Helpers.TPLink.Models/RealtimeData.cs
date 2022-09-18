d811b8e4b3d9180f40ec71f037ab7791a8b914b8
bob h <mail@bob.codes>
2022-09-18 03:06:11 +0100

    tidying

--- Helpers.TPLink.Models/RealtimeData.cs
+++ Helpers.TPLink.Models/RealtimeData.cs
@@ -1,8 +1,7 @@
-﻿namespace Helpers.TPLink.Models
+﻿namespace Helpers.TPLink.Models;
+
+public record RealtimeData(double Amps, double Volts, double Watts)
 {
-	public record RealtimeData(double Amps, double Volts, double Watts)
-	{
-		public static explicit operator RealtimeData(Generated.ResponseObject.EmeterObject.RealtimeDataObject generated)
-			=> new(generated.current_ma / 1_000d, generated.voltage_mv / 1_000d, generated.power_mw / 1_000d);
-	}
+	public static explicit operator RealtimeData(Generated.ResponseObject.EmeterObject.RealtimeDataObject generated)
+		=> new(generated.current_ma / 1_000d, generated.voltage_mv / 1_000d, generated.power_mw / 1_000d);
 }
