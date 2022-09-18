107b55fb7c55b7484bc70b02a96f6a1887544064
bob h <mail@bob.codes>
2022-09-18 02:20:16 +0100

    tidying Helpers.Json
    ...disabling EnablePreviewFeatures

--- Helpers.Json/Converters/JsonUriConverter.cs
+++ Helpers.Json/Converters/JsonUriConverter.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace Helpers.Json.Converters
-{
-	public class JsonUriConverter : JsonConverter<Uri>
-	{
-		public override Uri? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-		{
-			return Uri.TryCreate(reader.GetString(), UriKind.RelativeOrAbsolute, out var result)
-				? result
-				: default;
-		}
+namespace Helpers.Json.Converters;
 
-		public override void Write(Utf8JsonWriter writer, Uri value, JsonSerializerOptions options)
-		{
-			writer.WriteStringValue(value.ToString());
-		}
+public class JsonUriConverter : JsonConverter<Uri>
+{
+	public override Uri? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		var s = reader.GetString();
+		return s is not null && Uri.TryCreate(s, UriKind.RelativeOrAbsolute, out var result)
+			? result
+			: null;
+	}
+
+	public override void Write(Utf8JsonWriter writer, Uri value, JsonSerializerOptions options)
+	{
+		writer.WriteStringValue(value.ToString());
 	}
 }
