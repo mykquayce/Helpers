3d55c6782616ecc3b745ca9c1849243897eadd70
bob h <mail@bob.codes>
2022-09-20 20:32:28 +0100

    removing superfluous tests

--- Helpers.TPLink.Tests/DependencyInjectionTests.cs
+++ Helpers.TPLink.Tests/DependencyInjectionTests.cs
@@ -24,22 +24,6 @@ public class DependencyInjectionTests
 	}
 
 	[Theory]
-	[InlineData(1)]
-	public void ConfigTests(ushort port)
-	{
-		var config = new Config(port);
-
-		var provider = new ServiceCollection()
-			.AddSingleton(config)
-			.AddTransient<ITPLinkClient, Concrete.TPLinkClient>()
-			.BuildServiceProvider();
-
-		var sut = provider.GetService<ITPLinkClient>();
-
-		Assert.NotNull(sut);
-	}
-
-	[Theory]
 	[InlineData(9_999)]
 	public async Task Test1(ushort port)
 	{
