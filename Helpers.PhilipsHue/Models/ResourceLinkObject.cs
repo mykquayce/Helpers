7bcc36dba9880aa35c36039484b5b5f32bd1cb25
bob h <mail@bob.codes>
2022-09-27 15:08:59 +0100

    completely removing old library

--- Helpers.PhilipsHue/Models/ResourceLinkObject.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Helpers.PhilipsHue.Models;
-
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "3rd party")]
-public record ResourceLinkObject(
-	string name, string description, string type,
-	int classid, string owner, bool recycle, IReadOnlyList<string> links);
