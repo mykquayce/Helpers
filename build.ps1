8c21de5e7ffeab8d883238d229292080f3ca91a0
bob h <mail@bob.codes>
2022-11-09 15:10:15 +0000

    improving build script

--- build.ps1
+++ build.ps1
@@ -1,10 +1,22 @@
+$start = Get-Date -Format "o"
+
+# update the base image
 docker pull mcr.microsoft.com/dotnet/sdk:7.0 `
 	|| exit 1
 
+# build
 docker build `
 	--build-arg "NuGetServerApiKey=${env:NuGetServerApiKey}" `
 	--no-cache `
 	--secret id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt `
 	.
 
-docker image prune --force
+# remove "dangling" images created since the script began
+$ids = docker image ls --filter dangling=true --format '{{.ID}}'
+
+foreach ($id in $ids) {
+	$created = docker image inspect --format '{{.Created}}' $id
+	if ($created -gt $start) {
+		docker image rm $id
+	}
+}
