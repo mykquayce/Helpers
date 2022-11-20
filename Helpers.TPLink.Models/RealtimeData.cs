ad4614738754e1623d5cde9b92739a23420eb382
bob h <mail@bob.codes>
2022-11-20 16:14:51 +0000

    models

--- Helpers.TPLink.Models/RealtimeData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Helpers.TPLink.Models;
-
-public record RealtimeData(double Amps, double Volts, double Watts)
-{
-	public static explicit operator RealtimeData(Generated.ResponseObject.EmeterObject.RealtimeDataObject generated)
-		=> new(generated.current_ma / 1_000d, generated.voltage_mv / 1_000d, generated.power_mw / 1_000d);
-}
