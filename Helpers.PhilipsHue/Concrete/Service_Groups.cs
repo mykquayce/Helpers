c6f9c151236121c4704adbcfc7748435edd023bd
bob hall <myk_quayce@hotmail.com>
2023-05-08 10:46:28 +0100

    fixing three service endpoints not using the alias cache

--- Helpers.PhilipsHue/Concrete/Service_Groups.cs
+++ Helpers.PhilipsHue/Concrete/Service_Groups.cs
@@ -4,42 +4,19 @@ public partial class Service
 {
 	public async Task<bool> GetGroupPowerAsync(string alias, CancellationToken cancellationToken = default)
 	{
-		var groups = _client.GetGroupsAsync(cancellationToken);
-		await foreach (var (name, id) in groups)
-		{
-			if (string.Equals(alias, name, StringComparison.OrdinalIgnoreCase))
-			{
-				return await _client.GetGroupPowerAsync(id, cancellationToken);
-			}
-		}
-		throw new ArgumentOutOfRangeException(nameof(alias), alias, $"group {alias} not found");
+		var id = await ResolveGroupNameAsync(alias, cancellationToken);
+		return await _client.GetGroupPowerAsync(id, cancellationToken);
 	}
 
 	public async Task SetGroupPowerAsync(string alias, bool on, TimeSpan transition, CancellationToken cancellationToken = default)
 	{
-		var groups = _client.GetGroupsAsync(cancellationToken);
-		await foreach (var (name, id) in groups)
-		{
-			if (string.Equals(alias, name, StringComparison.OrdinalIgnoreCase))
-			{
-				await _client.SetGroupPowerAsync(id, on, transition, cancellationToken);
-				return;
-			}
-		}
-		throw new ArgumentOutOfRangeException(nameof(alias), alias, $"group {alias} not found");
+		var id = await ResolveGroupNameAsync(alias, cancellationToken);
+		await _client.SetGroupPowerAsync(id, on, transition, cancellationToken);
 	}
 
 	public async Task ToggleGroupPowerAsync(string alias, TimeSpan transition, CancellationToken cancellationToken = default)
 	{
-		var groups = _client.GetGroupsAsync(cancellationToken);
-		await foreach (var (name, id) in groups)
-		{
-			if (string.Equals(alias, name, StringComparison.OrdinalIgnoreCase))
-			{
-				await _client.ToggleGroupPowerAsync(id, transition, cancellationToken);
-				return;
-			}
-		}
-		throw new ArgumentOutOfRangeException(nameof(alias), alias, $"group {alias} not found");
+		var id = await ResolveGroupNameAsync(alias, cancellationToken);
+		await _client.ToggleGroupPowerAsync(id, transition, cancellationToken);
 	}
 }
