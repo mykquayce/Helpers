dba9d71822d8b76fcb730c214929dc656a014cf6
bob h <mail@bob.codes>
2023-12-17 13:24:38 +0000

    rewrite

---
+++ Helpers.Reddit.Tests/DelegatingHandlers/RateLimitHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.RateLimiting;
+
+namespace System.Net.Http;
+
+public class RateLimitHandler(RateLimiter limiter) : DelegatingHandler
+{
+	protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		while (true)
+		{
+			using RateLimitLease lease = await limiter.AcquireAsync(permitCount: 1, cancellationToken);
+
+			if (lease.IsAcquired)
+			{
+				break;
+			}
+
+			await Task.Delay(millisecondsDelay: 200, cancellationToken);
+		}
+
+		return await base.SendAsync(request, cancellationToken);
+	}
+}
