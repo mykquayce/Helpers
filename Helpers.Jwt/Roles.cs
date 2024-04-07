a2de68401698a4164cb0b9d8cb0ed050babc8c34
mykquayce <myk_quayce@hotmail.com>
2024-04-07 10:00:40 +0100

    Jwt token vulnerabiltiy (#115)
    * bump nuget packages
    
    * tidying
    
    * bump version
    
    ---------
    
    Co-authored-by: bob h <mail@bob.codes>

---
+++ Helpers.Jwt/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Helpers.Jwt;
+
+[Flags]
+public enum Roles : byte
+{
+	Admin = 1,
+	Customer = 2,
+	User = 4,
+}
--- Helpers.Jwt/TokenFactory.cs
+++ Helpers.Jwt/TokenFactory.cs
@@ -1,69 +1,57 @@
-﻿using Helpers.Common;
-using Microsoft.AspNetCore.WebUtilities;
+﻿using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.IdentityModel.Tokens;
-using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 
-namespace Helpers.Jwt
+namespace Helpers.Jwt;
+
+public class TokenFactory
 {
-	public class TokenFactory
+	private static readonly TimeSpan _defaultExpiry = TimeSpan.FromMinutes(5);
+
+	private readonly JwtHeader _header;
+	private readonly string _issuer;
+	private readonly string _audience;
+
+	public TokenFactory(string key, string issuer, string audience)
 	{
-		private static readonly TimeSpan _defaultExpiry = TimeSpan.FromMinutes(5);
+		var encodedKey = WebEncoders.Base64UrlDecode(key);
+		var securityKey = new SymmetricSecurityKey(encodedKey);
+		var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
 
-		private readonly JwtHeader _header;
-		private readonly string _issuer;
-		private readonly string _audience;
-
-		public TokenFactory(string key, string issuer, string audience)
-		{
-			var encodedKey = WebEncoders.Base64UrlDecode(key);
-			var securityKey = new SymmetricSecurityKey(encodedKey);
-			var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
-
-			_audience = audience;
-			_header = new JwtHeader(credentials);
-			_issuer = issuer;
-		}
-
-		public string Generate(Roles roles = Roles.Admin, string? name = default, TimeSpan? expiry = default)
-		{
-			var expirySeconds = (int)DateTime.UtcNow
-				.Add(expiry ?? _defaultExpiry)
-				.Subtract(DateTime.UnixEpoch)
-				.TotalSeconds;
-
-			var payload = new JwtPayload
-			{
-				["exp"] = expirySeconds,
-				["iss"] = _issuer,
-				["aud"] = _audience,
-			};
-
-			payload.AddClaims(from r in roles.Expand()
-							  let s = r.ToString("G")
-							  let c = new Claim(ClaimTypes.Role, s)
-							  select c);
-
-			if (!string.IsNullOrWhiteSpace(name))
-			{
-				var claim = new Claim(ClaimTypes.NameIdentifier, name);
-				payload.AddClaim(claim);
-			}
-
-			var secToken = new JwtSecurityToken(_header, payload);
-			var handler = new JwtSecurityTokenHandler();
-
-			return handler.WriteToken(secToken);
-		}
+		_audience = audience;
+		_header = new JwtHeader(credentials);
+		_issuer = issuer;
 	}
 
-	[Flags]
-	public enum Roles : byte
+	public string Generate(Roles roles = Roles.Admin, string? name = default, TimeSpan? expiry = default)
 	{
-		Admin = 1,
-		Customer = 2,
-		User = 4,
+		var expirySeconds = (int)DateTime.UtcNow
+			.Add(expiry ?? _defaultExpiry)
+			.Subtract(DateTime.UnixEpoch)
+			.TotalSeconds;
+
+		var payload = new JwtPayload
+		{
+			["exp"] = expirySeconds,
+			["iss"] = _issuer,
+			["aud"] = _audience,
+		};
+
+		payload.AddClaims(from r in roles.Expand()
+						  let s = r.ToString("G")
+						  let c = new Claim(ClaimTypes.Role, s)
+						  select c);
+
+		if (!string.IsNullOrWhiteSpace(name))
+		{
+			var claim = new Claim(ClaimTypes.NameIdentifier, name);
+			payload.AddClaim(claim);
+		}
+
+		var secToken = new JwtSecurityToken(_header, payload);
+		var handler = new JwtSecurityTokenHandler();
+
+		return handler.WriteToken(secToken);
 	}
 }
