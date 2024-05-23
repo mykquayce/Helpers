3ad6f6a321fe50c3c09329be1a21bb0355a416d6
bob h <mail@bob.codes>
2024-05-23 10:05:08 +0100

    fixing / tidying build script

--- build.ps1
+++ build.ps1
@@ -1,15 +1,14 @@
 $start = Get-Date -Format "o"
 
 # update the base image
-docker pull mcr.microsoft.com/dotnet/sdk:8.0 `
-	|| exit 1
+docker pull mcr.microsoft.com/dotnet/sdk:8.0
+if (!$?) { exit 1; }
 
 # build
-$secret = 'id=ca_crt,src={0}\.aspnet\https\ca.crt' -f ${env:userprofile}
 docker build `
 	--build-arg "NuGetServerApiKey=${env:NuGetServerApiKey}" `
 	--no-cache `
-	--secret $secret `
+	--secret "id=ca_crt,src=${env:userprofile}\.aspnet\https\ca.crt" `
 	.
 
 # remove "dangling" images created since the script began
